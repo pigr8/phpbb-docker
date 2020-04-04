3a9e6fd2f43baf350adca64b88dff3af810d8c37
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-04 11:52:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,5 @@ set -euo pipefail
 
 cp -r -f /usr/share/zoneinfo/$TZ /etc/localtime
 
-if [ "$RM_INSTALL" = "true" ]
-  then
-    rm -rf /var/www/phpBB3/install/
-    echo "Removed Install Directory"
-  else
-   echo "Install Directory retained."
-fi
-
 unset TZ RM_INSTALL
 exec "$@"
